a66a0a61af6258ab151cfc41cb54729df34b439b
conniefry <constance_fry@cable.comcast.com>
2018-10-17 12:44:39 -0700

    some file renames

--- docs/sparkScript.js
+++ docs/sparkScript.js
@@ -9,31 +9,6 @@ var getAll = function (selector, scope) {
   return scope.querySelectorAll(selector);
 };
 
-// setup typewriter effect in the terminal demo
-/*if (document.getElementsByClassName('demo').length > 0) {
-  var i = 0;
-  var txt = `scribbler
-            [Entry mode; press Ctrl+D to save and quit; press Ctrl+C to quit without saving]
-
-            ###todo for new year dinner party
-
-            - milk
-            - butter
-            - green onion
-            - lots and lots of kiwis 🥝`;
-  var speed = 60;
-
-  function typeItOut () {
-    if (i < txt.length) {
-      document.getElementsByClassName('demo')[0].innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeItOut, speed);
-    }
-  }
-
-  setTimeout(typeItOut, 1800);
-}*/
-
 // toggle tabs on codeblock
 window.addEventListener("load", function() {
   // get all tab_containers in the document
