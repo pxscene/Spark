86da81eec133e5368051e6d43cd39d3bca44af73
ashwinikalokhe <41405986+ashwinikalokhe@users.noreply.github.com>
2019-05-06 12:42:35 -0400

    gif tests (#64)
    * gif test
    
    * gif test
    
    * Gif tests
    
    * Delete picturepileGif.js~
    
    * tests.json: added picturepilegif.js
    
    * Added tests for GIF loader
    
    * Gift loader changes
    
    * fixed name of file
    
    * Delete picturepileGif.js~
    
    * Fixed tests when gif support is off
    
    * Clean up
    
    * added capabilities in use and changed paths
    
    * Update gallery.js
    
    * Update gallery.js
    
    * clean up
    
    * clean up
    
    * clean up
    
    * Removed tests.json
    
    * clean up
    
    * Update tests.json
    
    * Update tests.json
    
    * Added test to test ImageAResource loader
    
    * fixed test2
    
    * fixed test2

---
+++ examples/gallery/agif1.js
@@ -0,0 +1,48 @@
+"use strict";
+
+px.import("px:scene.1.js").then(function (scene) {
+
+  if (scene.capabilities.graphics.gif == false)
+   	{
+	console.error("Gif support is disabled");
+	return;
+	}
+  var basePackageUri = px.getPackageBaseFilePath();
+  var url = basePackageUri + "/images/gifs/Spark_equalizerSVG.gif";
+
+  var i = scene.create({ t: "imageA", url: url, parent: scene.root });
+  var it = void 0;
+
+  i.ready.then(function () {
+    var iw = scene.create({ t: "imageA", url: url, parent: scene.root, stretchX: 1 });
+    iw.ready.then(function (o) {
+      iw.x = i.w;iw.w = i.w * 2;
+    });
+    var ih = scene.create({ t: "imageA", url: url, parent: scene.root, stretchY: 1 });
+    ih.ready.then(function (o) {
+      ih.y = i.h;ih.h = i.h * 2;
+    });
+    it = scene.create({ t: "imageA", url: url, parent: scene.root, stretchX: 2, stretchY: 2 });
+    it.ready.then(function (o) {
+      it.x = i.w;it.y = i.h;it.x = i.w;it.w = i.w * 2;it.y = i.h;it.h = i.h * 2;
+    });
+  });
+
+  scene.root.on("onChar", function (e) {
+    if (it) {
+      switch (e.charCode) {
+        case '0'.charCodeAt(0):
+          it.stretchX = 0;it.stretchY = 0;
+          break;
+        case '1'.charCodeAt(0):
+          it.stretchX = 1;it.stretchY = 1;
+          break;
+        case '2'.charCodeAt(0):
+          it.stretchX = 2;it.stretchY = 2;
+          break;
+      }
+    }
+  });
+}).catch(function (e) {
+  console.error("Import failed for fancy.js: " + e);
+});
