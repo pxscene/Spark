cfdb4ab878c7237a0a1e8eca273ee3c9c447dd75
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-06-17 11:46:30 -0400

    clean up

--- tests-ci/tests/test_gifLoader_pxImage.js
+++ tests-ci/tests/test_gifLoader_pxImage.js
@@ -1,7 +1,7 @@
 px.import({scene:"px:scene.1.js",
-           assert:"../test-run/assert.js",
-           shots:"../test-run/tools_screenshot.js",
-           manual:"../test-run/tools_manualTests.js"}).then( function ready(imports) {
+assert:"../test-run/assert.js",
+shots:"../test-run/tools_screenshot.js",
+manual:"../test-run/tools_manualTests.js"}).then( function ready(imports) {
 
 var scene = imports.scene;
 var root = scene.root;
@@ -25,84 +25,88 @@ var url = basePackageUri + "/images/Spark_equalizerSVG.gif";
 /**                                                                   */
 /**********************************************************************/
 var beforeStart = function() {
-  return new Promise(function(resolve, reject) {
-    resolve("test_gifLoader_pxImage.js beforeStart");
-  });
+return new Promise(function(resolve, reject) {
+resolve("test_gifLoader_pxImage.js beforeStart");
+});
 }
 
 var doScreenshotComparison = function(name, resolve, reject) 
 {
-  testText.text="ScreenShot"+name;
-    var results = rectMeasurementResults();
-    shots.validateScreenshot(basePackageUri+"/images/screenshot_results/"+testPlatform+"/gifLoader_pxImage_test"+name+".png", false).then(function(match){
-        console.log("test result is match: "+match);
-        results.push(assert(match == true, "screenshot comparison for "+name+" failed\n"+basePackageUri+"/images/screenshot_results/gifLoader_tests_"+name+".png"));
-        resolve(results);
-     // });
-    }).catch(function(err) {
-        results.push(assert(false, "screenshot comparison for "+name+" failed due to error: "+err));
-        resolve(results);
-    });
+testText.text="ScreenShot"+name;
+var results = rectMeasurementResults();
+shots.validateScreenshot(basePackageUri+"/images/screenshot_results/"+testPlatform+"/gifLoader_pxImage_test"+name+".png", false).then(function(match){
+console.log("test result is match: "+match);
+results.push(assert(match == true, "screenshot comparison for "+name+" failed\n"+basePackageUri+"/images/screenshot_results/gifLoader_tests_"+name+".png"));
+resolve(results);
+// });
+}).catch(function(err) {
+results.push(assert(false, "screenshot comparison for "+name+" failed due to error: "+err));
+resolve(results);
+});
 }
 var tests = {
 
-  test1: function() {
-	if (!isGifLoaderEnabled)
-	{   
-			console.log(scene.capabilities.graphics.gif == undefined ? "No GIF support in this Spark build!" : "GIF version support is not compatible with this example; example requires at least version 2")
-			return new Promise(function(resolve, reject) { resolve(assert(!isGifLoaderEnabled));
-		});
-	}
-	else
-	{
-  	var img = scene.create({ t: "image", url: url, parent: scene.root });
+test1: function() {
+if (!isGifLoaderEnabled)
+{   
+ console.log(scene.capabilities.graphics.gif == undefined ? "No GIF support in this Spark build!" : "GIF version support is not compatible with this example; example requires at least version 2")
+ return new Promise(function(resolve, reject) { resolve(assert(!isGifLoaderEnabled));
+});
+}
+else
+{
+var img = scene.create({ t: "image", url: url, parent: scene.root });
 
-	return new Promise(function(resolve, reject) {
-	    img.ready.then(function() {
-	      if(doScreenshot) 
-			{
-			setTimeout( function() {
-				doScreenshotComparison("test1", resolve)
-			}, timeoutForScreenshot);
-			} 
-	      else 
-			resolve(assert(isGifLoaderEnabled) , "test_gifLoader_pxImage: Failed to load file");
-	    },
-		function(msg){ // reject
-			resolve(assert(false, "test_gifLoader_pxImage: Failed to load file") );
-		});
-	  });
-	}
-	},
+return new Promise(function(resolve, reject) {
+img.ready.then(function() {
+if(doScreenshot) 
+{
+setTimeout( function() {
+ doScreenshotComparison("test1", resolve)
+}, timeoutForScreenshot);
+} 
+else 
+resolve(assert(isGifLoaderEnabled) , "test_gifLoader_pxImage: Failed to load file");
+},
+function(msg){ // reject
+ resolve(assert(false, "test_gifLoader_pxImage: Failed to load file") );
+});
+});
+}
+},
 test2: function() {
-	if (!isGifLoaderEnabled)
-	{   
-		console.log(scene.capabilities.graphics.gif == undefined ? "No GIF support in this Spark build!" : "GIF version support is not compatible with this example; example requires at least version 2")
-		return new Promise(function(resolve, reject) { resolve(assert(!isGifLoaderEnabled));
-			});
-	}
-	else
-	{
-      var imgres = scene.create({t:'imageResource', url:url, parent: scene.root});
-	  var img = scene.create({ t: "image", resource:imgres, parent: scene.root });
-	  return new Promise(function(resolve, reject) {
-		img.ready.then(function() {
-			if(doScreenshot) 
-			{
-			setTimeout( function() {
-				doScreenshotComparison("test2", resolve)
-			}, timeoutForScreenshot);
-			} 
-			else 
-				resolve(assert(isGifLoaderEnabled) , "test_gifLoader_pxImage: Failed to load file");
-			},
-			function(msg){ // reject
-				resolve(assert(false, "test_gifLoader_pxImage: Failed to load file") );
-			});
-		});
-	}
-  }
+if (!isGifLoaderEnabled)
+{   
+console.log(scene.capabilities.graphics.gif == undefined ? "No GIF support in this Spark build!" : "GIF version support is not compatible with this example; example requires at least version 2")
+return new Promise(function(resolve, reject) { resolve(assert(!isGifLoaderEnabled));
+ });
+}
+else
+{
+var imgres = scene.create({t:'imageResource', url:url, parent: scene.root});
+
+ var img = scene.create({ t: "image", resource:imgres, parent: scene.root });
+		 
+ return new Promise(function(resolve, reject) {
+		 img.ready.then(function() {
+
+						 if(doScreenshot) 
+			 {
+		 setTimeout( function() {
+			 doScreenshotComparison("test2", resolve)
+		 }, timeoutForScreenshot);
+			 } 
+			 else 
+				 resolve(assert(isGifLoaderEnabled) , "test_gifLoader_pxImage: Failed to load file");
+		 },
+		 function(msg){ // reject
+			 resolve(assert(false, "test_gifLoader_pxImage: Failed to load file") );
+		 });
+	 });
  }
+}
+
+}
 
 module.exports.beforeStart = beforeStart;
 module.exports.tests = tests;
@@ -110,10 +114,10 @@ module.exports.tests = tests;
 
 if(manualTest === true) {
 
-  manual.runTestsManually(tests, beforeStart);
+manual.runTestsManually(tests, beforeStart);
 
 }
 
 }).catch( function importFailed(err){
-  console.error("Import failed for test_gifLoader_pxImage.js: " + err)
+console.error("Import failed for test_gifLoader_pxImage.js: " + err)
 });
