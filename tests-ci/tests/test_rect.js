add36ba0a0e8a9138501247bd8fe3498ff162115
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-06-28 12:30:55 -0400

    Cleanup

--- tests-ci/tests/test_rect.js
+++ tests-ci/tests/test_rect.js
@@ -4,5 +4,5 @@ var basePackageUri = px.getPackageBaseFilePath();
 var rect = scene.create({t:"rect", parent: root, w:500, h:500, fillColor:0xFF0000ff});
 
 }).catch( function importFailed(err){
-  console.error("Import failed for cliptest.js: " + err)
+  console.error("Import failed for test_rect.js: " + err)
 });
