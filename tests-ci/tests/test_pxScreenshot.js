c6a9dd8d21efa1d165419cfcfe4c7faddbc6fbc9
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-07-19 10:36:46 -0400

    updated console log

--- tests-ci/tests/test_pxScreenshot.js
+++ tests-ci/tests/test_pxScreenshot.js
@@ -32,13 +32,11 @@ var tests = {
         var base64String = stage.screenshot("image/png;base64");
         var expected64String="data:image/png;base64";
         console.log("test_pxScreenshot:"+base64String);
-        var isValid = ( base64String.indexOf(expected64String) == 0)
-        console.log(assert(isValid,"screen shot base64String!=expected64String"));
-      
+        var isValid = ( base64String.indexOf(expected64String) == 0);
         results.push(assert(isValid,"screen shot base64String!=expected64String"));
       }, function failure() {
-        console.log(assert(false,"test_pxScreenshot failed on loading stage!"));
-        results.push(assert(false,"test_pxScreenshot failed on loading stage!"));
+        console.log("test_pxScreenshot failed!");
+        results.push("test_pxScreenshot failed!");
       }).then(function(error) {
         console.log("returning result");
         resolve(results);
