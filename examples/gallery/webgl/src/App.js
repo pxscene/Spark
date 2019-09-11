db81150f6e32c4c35a771194e5bb69ed3c6e7a5c
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-09-11 10:20:54 -0400

    Added samples

--- examples/gallery/webgl/src/App.js
+++ examples/gallery/webgl/src/App.js
@@ -1,11 +1,12 @@
 export default class MyApp extends ux.App {
     static _template() {
         return {
-            BackgroundImage: { src: MyApp.getPath("background.png")},
-            MainImage: { src: MyApp.getPath("rockies.jpeg")},
-            Text: {color: 0xff00B74F, text: {text: "Hello World with mode 1", mode: 0}},
-            Text: {color: 0xff00B74F, text: {text: "Hello World with mode 0", mode: 1}},
-            Circle: {src: MyApp.getPath("rockies.jpeg"), color: 0xFFAAAAAA, mode: 0, pivot: 0, scale: 1, w: 1280, h: 720}
+            Image1: { src: MyApp.getPath("pebbles.jpg"), w:450, h:450},
+            Image2: {src: MyApp.getPath("pebbles.jpg"), color: 0xFFAAAAAA, mode: 0, pivot: 0, scale: 1, w:450, h:450, x: 500},
+            Image3: {src: MyApp.getPath("pebbles.jpg"), color: 0xFFAAAAAA, mode: 1, pivot: 0, scale: 1, w:450, h:450, y: 500},
+            Text1: {color: 0xff00B74F, text: {text: "Hello World"}},
+            Text2: {color: 0xff00B74F, text: {text: "Hello World with mode 1"}, mode: 0, x:500},
+            Text3: {color: 0xff00B74F, text: {text: "Hello World with mode 0"}, mode: 1, y:500},
         };
     }
 }
