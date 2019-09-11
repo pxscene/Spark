7b0451628a3c1cfadd53ce88813bb0fb427893af
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-09-11 09:54:49 -0400

    Added sample text with mode 0 and 1

--- examples/gallery/webgl/src/App.js
+++ examples/gallery/webgl/src/App.js
@@ -3,8 +3,9 @@ export default class MyApp extends ux.App {
         return {
             BackgroundImage: { src: MyApp.getPath("background.png")},
             MainImage: { src: MyApp.getPath("rockies.jpeg")},
-            Text: {color: 0xff00B74F, text: {text: "Hello World"}},
-            Circle: {src: MyApp.getPath("rockies.jpeg"), color: 0xFFAAAAAA, quadList: {mode: 0}, pivot: 0, scale: 1, w: 1280, h: 720}
+            Text: {color: 0xff00B74F, text: {text: "Hello World with mode 1", mode: 0}},
+            Text: {color: 0xff00B74F, text: {text: "Hello World with mode 0", mode: 1}},
+            Circle: {src: MyApp.getPath("rockies.jpeg"), color: 0xFFAAAAAA, mode: 0, pivot: 0, scale: 1, w: 1280, h: 720}
         };
     }
 }
