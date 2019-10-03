12c4f33cfcfa2c067b4d69386fb7dc6e678ab565
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2019-10-03 20:17:58 +0300

    replace tinyurl.com with is.gd

--- tests-ci/tests/test_storage.js
+++ tests-ci/tests/test_storage.js
@@ -19,7 +19,7 @@ importsPromise.then(im => {
 });
 
 const child_url = `${px.getPackageBaseFilePath()}/helpers/test_childAppStorage.js`;
-const tiny_url = 'https://tinyurl.com/yxrf6ngq'; // redirects to test_childAppStorage.js
+const tiny_url = 'https://is.gd/tFDMQi'; // -> https://www.sparkui.org/tests-ci/tests/helpers/test_childAppStorage.js
 
 module.exports.tests = {};
 
