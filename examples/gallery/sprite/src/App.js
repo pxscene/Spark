3970e6d1e50661d659ffb6ff80c2db5c323b85e7
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-10-07 15:06:23 -0400

    Fixed measurements

--- examples/gallery/sprite/src/App.js
+++ examples/gallery/sprite/src/App.js
@@ -17,6 +17,14 @@ export default class MyApp extends ux.App {
                w:450, 
                h:450
            },
+           SpriteQuadList3: {
+              id: 2,
+              isSharable: true,
+              type: lng.SpriteQuadList, 
+              src: MyApp.getPath("pebbles.jpg"), 
+              w:450, 
+              h:450
+          },
         };
         return {
             
@@ -37,7 +45,13 @@ export default class MyApp extends ux.App {
                 bottom:0.1, left:0.05, top:80, right:60, x:200, y:0},
             ImageTexture6: { type:lng.ImageTexture, 
                 quadsList: shared.SpriteQuadList1, 
-               bottom:128, left:0.05, top:215, right:84.7, x:200, y:100},
+               bottom:128, left:0.05, top:215, right:84, x:200, y:100},
+            ImageTexture7: { type:lng.ImageTexture,
+                quadsList: shared.SpriteQuadList3, 
+                bottom:0.1, left:0.05, top:80, right:60, x:300, y:0},
+            ImageTexture8: { type:lng.ImageTexture, 
+                quadsList: shared.SpriteQuadList3, 
+                bottom:128, left:0.05, top:215, right:84, x:300, y:100},
         };
     }
 }
