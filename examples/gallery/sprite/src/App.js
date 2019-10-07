eab6172c8e0e5ac13e39ebb5ead8fc4112709bc3
AshwiniKalokhe <ashwini_kalokhe@comcast.com>
2019-10-07 14:46:00 -0400

    Fixed measurements

--- examples/gallery/sprite/src/App.js
+++ examples/gallery/sprite/src/App.js
@@ -25,19 +25,19 @@ export default class MyApp extends ux.App {
                 bottom:0.1, left:0.05, top:80, right:60, x:0, y:0},
             ImageTexture2: { type:lng.ImageTexture, 
                 quadsList: shared.SpriteQuadList1, 
-                bottom:128, left:0.05, top:215.1, right:84.7, x:0, y:100},
+                bottom:128, left:0.05, top:215, right:84.7, x:0, y:100},
             ImageTexture3: { type:lng.ImageTexture,
                 quadsList: shared.SpriteQuadList2, 
                 bottom:0.1, left:0.05, top:80, right:60, x:100, y:0},
             ImageTexture4: { type:lng.ImageTexture, 
                 quadsList: shared.SpriteQuadList2, 
-                bottom:0.1, left:0.05, top:80, right:60, x:100, y:100},
+                bottom:128, left:0.05, top:215, right:84.7, x:100, y:100},
             ImageTexture5: { type:lng.ImageTexture,
                 quadsList: shared.SpriteQuadList1, 
                 bottom:0.1, left:0.05, top:80, right:60, x:200, y:0},
             ImageTexture6: { type:lng.ImageTexture, 
                 quadsList: shared.SpriteQuadList1, 
-                bottom:0.1, left:0.05, top:80, right:60, x:200, y:100},
+               bottom:128, left:0.05, top:215, right:84.7, x:200, y:100},
         };
     }
 }
