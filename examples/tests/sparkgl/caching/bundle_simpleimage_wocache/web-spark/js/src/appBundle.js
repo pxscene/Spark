bf1b827b2d587dd4bc4996d4e43d02c0293788be
Madana Gopal Thirumalai <madanagopal_thirumalai@comcast.com>
2020-02-05 08:31:50 +0530

    added caching tests

---
+++ examples/tests/sparkgl/caching/bundle_simpleimage_wocache/web-spark/js/src/appBundle.js
@@ -0,0 +1,25 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.appBundle = factory());
+}(this, (function () { 'use strict';
+
+    console.log("LOADED bundle image app without using cache");
+
+    class MyApp extends ux.App {
+        static _template() {
+            return {
+                BackgroundImage: { src: MyApp.getPath("background.png")},
+                MainImage: { src: MyApp.getPath("rockies.jpeg")},
+                Text: {color: 0xff00B74F, text: {text: "Hello World"}}
+            };
+        }
+    }
+
+    MyApp.COLORS = {
+        BACKGROUND: 0xff282e32
+    };
+
+    return MyApp;
+
+})));
