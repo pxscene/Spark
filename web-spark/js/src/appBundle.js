23320c8cd3d00d4e7a2ef702f2cfc7ac818802d6
FitzerIRL <Hugh_Fitzpatrick@hotmail.com>
2020-02-07 11:33:23 -0500

    Updates to promote Spark 2.0

---
+++ web-spark/js/src/appBundle.js
@@ -0,0 +1,56 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.appBundle = factory());
+}(this, (function () { 'use strict';
+
+    class MyApp extends ux.App {
+        static _template() {
+            var hh = 230; //400
+            return {
+
+               Bg: { x: 0, y: 0, w: 400, h: hh, rect: true, color: 0xFF8888ff },
+               Ball: {
+                  x: 275, y: 275, w: 100, h: 100, src: MyApp.getPath("ball.png")
+               },
+               Text1: { x: 50, y: hh - 10, mountX: 0, mountY: 1, fontFace: 'RobotoRegular',
+                  text: { fontSize: 16, textColor: 0xFFffffff, text: 'Spark Platform' }
+               },
+               Spark: {
+                  x: 40, y: hh - 10, w: 24, h: 24, mount: 1, src: MyApp.getPath("Spark_logo256px.png")
+               },
+               Text2: { x: 400 - 10, y: hh - 10, mount: 1, fontFace: 'RobotoRegular',
+                  text: { fontSize: 16, textColor: 0xFFffffff, text: 'Lightning App' }
+               },
+               Lightning: {
+                  x: 400 - 100, y: hh + 5, w: 50, h: 50, mount: 1, src: MyApp.getPath("Lightning.png")
+               },
+            }
+         }
+
+         _init()
+         {
+            var ww = 400 * 0.75;
+            var hh = 230 / 2;
+            this._anim = this.tag('Ball').animation({
+            duration: 6,
+            repeat: -1,
+            stopMethod: 'immediate',
+               actions:[
+                  {p: 'scale', v: { 0: {v: 1, s: 1}, 0.5: {v: -1, s: 1}, 1: {v: 1, s: 1}}},
+                  {p:     'x', v: {0: 50, 0.25: ww * 0.5, 0.5: ww, 0.75: ww * 0.9, 1: 50}},
+                  {p:     'y', v: {0: 10, 0.25: hh , 0.5: 50, 0.75: hh, 1: 10 }}
+               ]
+            });
+
+         this._anim.start();
+         }
+    }
+
+    MyApp.COLORS = {
+        BACKGROUND: 0xff282e32
+    };
+
+    return MyApp;
+
+})));
